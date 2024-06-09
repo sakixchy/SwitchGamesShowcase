38d59954ff7788cf6b4d00d910f28da52b43e08d
Saki Chy <sakibchowdhury79@gmail.com>
2024-06-09 09:40:02 +0000

    add routing to navbar and style navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,26 @@
-import './App.css';
+import styles from './App.module.css';
 import NavBar from "./components/NavBar";
+import Container from 'react-bootstrap/Container';
+import { Route, Switch } from 'react-router-dom';
 
 
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
+    <div className={styles.App}>
+      <NavBar/>
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={()=> <h1>Home Page</h1>} />
+          <Route exact path="/signin" render={()=> <h1>Sign In</h1>} />
+          <Route exact path="/signup" render={()=> <h1>Sign Up</h1>} />
+          <Route exact path="/games" render={()=> <h1>Games</h1>} />
+          <Route exact path="/rentals" render={()=> <h1>Rentals</h1>} />
+          <Route exact path="/profile" render={()=> <h1>Profile</h1>} />
+          <Route render={()=> <p>Page not found!</p>} />
+        </Switch>
+      </Container>
+
     </div>
   );
 }
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,19 +1,49 @@
 import React from 'react'
 import { Navbar, Container, Nav } from 'react-bootstrap';
-import logo from '../assets/images/logo.png'
+import logo from '../assets/images/logo.png';
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <div>
-        <Navbar bg="danger" expand="md" fixed="top">
+        <Navbar className={styles.NavBar} expand="md" fixed="top">
         <Container>
+          <NavLink to="/">
+            <Navbar.Brand>
             <img src={logo} alt="logo" height="45" />
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            </Navbar.Brand>
+          </NavLink>
+            <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <i className="fa-solid fa-angle-down"></i>
+            </Navbar.Toggle>
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto text-left">
-                <Nav.Link><i className="fas fa-home"></i>Home</Nav.Link>
-                <Nav.Link><i className="fas fa-sign-in-alt"></i>Sign In</Nav.Link>
-                <Nav.Link><i className="fas fa-user-plus"></i>Sign Up</Nav.Link>
+                <NavLink
+                exact
+                className={styles.NavLink}
+                activeClassName={styles.Active} 
+                to="/"><i className="fas fa-home"></i>Home</NavLink>
+                <NavLink
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+                to="/games"><i className="fa-solid fa-gamepad"></i>Games</NavLink>
+                <NavLink
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+                to="/rentals"><i className="fa-solid fa-clock"></i>Rentals</NavLink>
+                <NavLink
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+                to="/signin"><i className="fas fa-sign-in-alt"></i>Sign In</NavLink>
+                <NavLink
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+                to="/signup"><i className="fas fa-user-plus"></i>Sign Up</NavLink>
+                <NavLink
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+                to="/profile"><i className="fas fa-user-plus"></i>Profile</NavLink>
                 </Nav>      
             </Navbar.Collapse>
         </Container>
