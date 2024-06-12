d954ba896cd4adcc3d031d998cacb7769848b180
Saki Chy <sakibchowdhury79@gmail.com>
2024-06-12 11:02:32 +0000

    add logout and profile avatar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -3,11 +3,45 @@ import { Navbar, Container, Nav } from 'react-bootstrap';
 import logo from '../assets/images/logo.png';
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from 'react-router-dom';
-import { useCurrentUser } from '../contexts/CurrentUserContexts';
+import { useCurrentUser, useSetCurrentUser } from '../contexts/CurrentUserContexts';
+import Avatar from './Avatar';
+import axios from 'axios';
+
 
 const NavBar = () => {
   const currentUser = useCurrentUser()
-  const loggedInIcons = <>{currentUser?.username}</>
+  const setCurrentUser = useSetCurrentUser()
+
+  const handleSignOut = async () => {
+    try {
+      await axios.post('dj-rest-auth/logout/');
+      setCurrentUser(null)
+    }catch (err){
+      console.log(err)
+    }
+  }
+
+  const loggedInIcons = <>
+    <NavLink
+      className={styles.NavLink}
+      activeClassName={styles.Active}
+      to="/games"><i className="fa-solid fa-gamepad"></i>Games</NavLink>
+    <NavLink
+      className={styles.NavLink}
+      activeClassName={styles.Active}
+      to="/rentals"><i className="fa-solid fa-clock"></i>Rentals</NavLink>
+    <NavLink
+      exact
+      className={styles.NavLink} 
+      to="/"
+      onClick={handleSignOut}
+      ><i className="fas fa-sign-out-alt"></i>Sign Out</NavLink>
+    <NavLink
+      className={styles.NavLink}
+      to={`/profiles/${currentUser?.profile_id}`}>
+      <Avatar src={currentUser?.profile_image} text="Profile" height={40} />
+    </NavLink>
+  </>
   const loggedOutIcons = (
   <> 
     <NavLink
@@ -27,36 +61,26 @@ const NavBar = () => {
         <Container>
           <NavLink to="/">
             <Navbar.Brand>
-            <img src={logo} alt="logo" height="45" />
+              <img src={logo} alt="logo" height="45" />
             </Navbar.Brand>
           </NavLink>
-            <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
             <i className="fa-solid fa-angle-down"></i>
-            </Navbar.Toggle>
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto text-left">
-                <NavLink
+          </Navbar.Toggle>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto text-left">
+              <NavLink
                 exact
                 className={styles.NavLink}
-                activeClassName={styles.Active} 
-                to="/"><i className="fas fa-home"></i>Home</NavLink>
-                <NavLink
-                className={styles.NavLink}
                 activeClassName={styles.Active}
-                to="/games"><i className="fa-solid fa-gamepad"></i>Games</NavLink>
-                <NavLink
-                className={styles.NavLink}
-                activeClassName={styles.Active}
-                to="/rentals"><i className="fa-solid fa-clock"></i>Rentals</NavLink>
-                {currentUser ? loggedInIcons : loggedOutIcons}
-                <NavLink
-                className={styles.NavLink}
-                activeClassName={styles.Active}
-                to="/profile"><i className="fas fa-user-plus"></i>Profile</NavLink>
-                </Nav>      
-            </Navbar.Collapse>
+                to="/">
+                <i className="fas fa-home"></i>Home
+              </NavLink>
+              {currentUser ? loggedInIcons : loggedOutIcons}
+            </Nav>
+          </Navbar.Collapse>
         </Container>
-        </Navbar>
+      </Navbar>
     </div>
   )
 }
